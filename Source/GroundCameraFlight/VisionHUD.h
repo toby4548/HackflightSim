9a33fd8358340ebeff0a87a9f4ec693fb3c9c8cf
simondlevy <simon.d.levy@gmail.com>
2018-04-09 19:46:12 -0400

    More VisionHUD work

--- Source/GroundCameraFlight/VisionHUD.h
+++ Source/GroundCameraFlight/VisionHUD.h
@@ -3,7 +3,10 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Engine.h"
 #include "GameFramework/HUD.h"
+#include "Engine/TextureRenderTarget2D.h"
+
 #include "VisionHUD.generated.h"
 
 /**
@@ -14,7 +17,27 @@ class GROUNDCAMERAFLIGHT_API AVisionHUD : public AHUD
 {
 	GENERATED_BODY()
 	
+	AVisionHUD();
+
+	virtual void DrawHUD() override;
+
+	const float LEFTX  = 45.f;
+	const float TOPY   = 90.f;
+	const float WIDTH  = 256.f;
+	const float HEIGHT = 128.f;
 	
-	
-	
+	const FLinearColor BORDER_COLOR = FLinearColor::Yellow;
+	const float BORDER_WIDTH = 2.0f;
+
+	void drawBorder(float lx, float uy, float rx, float by);
+
+	// Access to Vision camera
+	UTextureRenderTarget2D* VisionTextureRenderTarget;
+	FRenderTarget* VisionRenderTarget;
+	TArray<FColor> VisionSurfData;
+
+	// Support for vision algorithms
+	int rows;
+	int cols;
+	uint8_t* imagergb;
 };
